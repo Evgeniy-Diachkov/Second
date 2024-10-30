68bd8047730ca5185a03699969c3b107ae6f063b
Evgeny <j-z-b@ya.ru>
2024-10-31 01:45:01 +0300

    First commit

---
+++ src/Cookie.java
@@ -0,0 +1,9 @@
+public class Cookie {
+    public static void main(String[] args) {
+        byte a = 127;
+        byte b = 126;
+        byte c = 1;
+        Byte abc = (byte) (a+b+c);
+        System.out.println(abc);
+    }
+}
